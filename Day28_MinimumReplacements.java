ab76c477bd8eff0aab4066f8ee695996051049c6
Yash Londhe <99034957+yashlondhe90960@users.noreply.github.com>
2023-02-13 22:51:03 +0530

    Create Day28_MinimumReplacements.java

---
+++ Day28_MinimumReplacements.java
@@ -0,0 +1,32 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        int lenS = s.length();
+        int lenT = t.length();
+        int[][] dp = new int[lenS + 1][lenT + 1];
+        
+        for (int i = 0; i <= lenS; i++) {
+            for (int j = 0; j <= lenT; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                }
+            }
+        }
+        
+        System.out.println(dp[lenS][lenT]);
+    }
+}
