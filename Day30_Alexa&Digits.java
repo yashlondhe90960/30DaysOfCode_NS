3ed39789c70276d29862eabc79db61eacd5b5f04
Yash Londhe <99034957+yashlondhe90960@users.noreply.github.com>
2023-02-14 23:24:30 +0530

    Create Day30_Alexa&Digits.java

---
+++ Day30_Alexa&Digits.java
@@ -0,0 +1,21 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= 9; i++){
+            for(int j = 1; j <= 9; j++){
+                if(i * j == n){
+                    System.out.println("Yes");
+                    return;
+                }
+            }
+        }
+        System.out.println("No");
+    }
+}
